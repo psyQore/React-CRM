473aab0ff37054a502e9501a45973c481f238fdd
Juan Galarce <j6alarce@gmail.com>
2022-06-08 12:22:10 -0400

    Se aplica estilos tailwind al nav de layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,9 +2,27 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div>
-      <h1>Desde Layout</h1>
-
+    <div className="md:flex md:min-h-screen">
+      <div className="md:w-1/4 bg-blue-900 px-5 py-10">
+        <h2 className="text-4xl font-black text-center text-white">
+          CRM Cliuentes
+        </h2>
+        <nav className="mt-10">
+          <a
+            className="text-white text-2xl block mt-2 hover:text-blue-300"
+            href="/clients"
+          >
+            Clientes
+          </a>
+          <a
+            className="text-white text-2xl block mt-2 hover:text-blue-300"
+            href="/clients/new"
+          >
+            Nuevo Cliente
+          </a>
+        </nav>
+      </div>
+      <div className="md:w-3/4">2</div>
       <Outlet />
     </div>
   );
