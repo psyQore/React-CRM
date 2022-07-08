2668566f92478273747cd91478aca71a34da81e9
Juan Galarce <j6alarce@gmail.com>
2022-07-08 10:50:25 -0400

    Se instala formik y yup, se crea el formulario de nuevo cliente con formik y se añaden estilos

---
+++ src/components/FormNewClients.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+
+const FormnewClients = () => {
+  return (
+    <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto">
+      <h1 className="text-gray-600 font-bold text-xl uppercase text-center">
+        Agregar Cliente
+      </h1>
+
+      <Formik>
+        <Form className="mt-10">
+          <div className="mb-4">
+            <label className="text-gray-800" htmlFor="name">
+              Nombre:
+            </label>
+            <Field
+              id="name"
+              type="text"
+              className="mt-2 block w-full p-3 bg-gray-100"
+              placeholder="Nombre del cliente"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="text-gray-800" htmlFor="company">
+              Empresa:
+            </label>
+            <Field
+              id="company"
+              type="text"
+              className="mt-2 block w-full p-3 bg-gray-100"
+              placeholder="Nombre de la empresa"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="text-gray-800" htmlFor="email">
+              E-mail:
+            </label>
+            <Field
+              id="email"
+              type="email"
+              className="mt-2 block w-full p-3 bg-gray-100"
+              placeholder="Email del cliente"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="text-gray-800" htmlFor="phone">
+              Teléfono:
+            </label>
+            <Field
+              id="phone"
+              type="tel"
+              className="mt-2 block w-full p-3 bg-gray-100"
+              placeholder="Teléfono del cliente"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="text-gray-800" htmlFor="note">
+              Notas:
+            </label>
+            <Field
+              as="textarea"
+              id="note"
+              type="text"
+              className="mt-2 block w-full p-3 bg-gray-100 h-40"
+              placeholder="Notas del cliente"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Agregar Cliente"
+            className="mt-5 w-full bg-blue-800 p-3 text-white rounded-md font-bold uppercase text-lg hover:cursor-pointer"
+          />
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default FormnewClients;
--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Form from "../components/Form";
+import FormNeClients from "../components/FormNewClients";
 
 const NewClient = () => {
   return (
@@ -7,7 +7,7 @@ const NewClient = () => {
       <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
       <p className="mt-3">Llena los siguientes camos para registrar un nuevo cliente</p>
 
-      <Form/>
+      <FormNeClients/>
     </>
   );
 };
