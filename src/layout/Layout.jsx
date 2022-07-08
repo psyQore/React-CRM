e211835f780175b27dab226737e06b529cd435c6
Juan Galarce <j6alarce@gmail.com>
2022-07-08 07:43:20 -0400

    Se use useLocation de RR6 para detectar en que url nos encontramos

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,10 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const localtion = useLocation();
+
+  const currentUrl = localtion.pathname;
+
   return (
     <div className="md:flex md:min-h-screen">
       <div className="md:w-1/4 bg-blue-900 px-5 py-10">
@@ -9,13 +13,17 @@ const Layout = () => {
         </h2>
         <nav className="mt-10">
           <Link
-            className="text-white text-2xl block mt-2 hover:text-blue-300"
+            className={`${
+              currentUrl === "/clients" ? "text-blue-300" : "text-white"
+            } text-2xl block mt-2 hover:text-blue-300`}
             to="/clients"
           >
             Clientes
           </Link>
           <Link
-            className="text-white text-2xl block mt-2 hover:text-blue-300"
+            className={`${
+              currentUrl === "/clients/new" ? "text-blue-300" : "text-white"
+            } text-2xl block mt-2 hover:text-blue-300`}
             to="/clients/new"
           >
             Nuevo Cliente
