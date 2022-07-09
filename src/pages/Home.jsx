fbf1a9d248a134ef6112f56d6103e75ebca38de9
Juan Galarce <j6alarce@gmail.com>
2022-07-09 11:09:47 -0400

    Se hace la consulta para mostrar los clientes, se crea un state para guardarlos en el componente home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from "react"
 
 const Home = () => {
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    const getClientsAPI = async () => {
+      try {
+        const url = "http://localhost:4000/clients";
+
+        const response = await fetch(url);
+        const result = await response.json();
+
+        setClients(result)
+      } catch (error) {
+        console.log(error);
+      }
+
+    }
+    getClientsAPI();
+  }, []);
+
   return (
     <div>
       <h1>Inicio</h1>
